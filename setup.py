6f1257d1bb4cbfe82ef1634da6bb29fa7662ccfc
Nick Tyler <tylern@canisius.edu>
2020-08-13 13:50:03 -0400

    Add install setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="cross_sections",
+    version="0.1",
+    description="Python Distribution Utilities",
+    author="Nick Tyler",
+    author_email="tylerns@email.sc.edu",
+    url="https://github.com/tylern4/crossSectionPlotter",
+    install_requires=["pandas", "numpy"],
+)
+
